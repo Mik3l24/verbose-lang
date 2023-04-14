5ff9206703c13e7e9e0713ead23eb1806a6a9765
Mik3l24 <michalczus@gmail.com>
2023-04-14 22:36:03 +0200

    Refactored example to use relex-style syntax

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include <cstdio>
 #include "lexer.h"
 
-int main()
+int main(int argc, char** argv) 
 {
-    yylex();
+    FILE* fd = stdin;
+    if(argc > 1 && (fd = fopen(argv[1], "r")) == NULL)
+        exit(EXIT_FAILURE);
+    if(fd == stdin) //Lexer only works when a file is open
+        exit(EXIT_FAILURE);
+    // create a lexer that consumes a file or reads stdin
+    Lexer lexer(fd);
+    // here we go!
+    lexer.lex();
+    // display the results
+    if(fd != stdin)
+        fclose(fd);
+    return 0;
 }
