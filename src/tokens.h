3e102d3e96518a0ee9682381883948f1296eee70
Mik3l24 <michalczus@gmail.com>
2023-04-20 20:09:07 +0200

    Added a convention for naming tokens

--- src/tokens.h
+++ src/tokens.h
@@ -1,12 +1,18 @@
 #ifndef VERBOSE_TOKENS_H
 #define VERBOSE_TOKENS_H
 
+// Token naming convention
+// keyword/key symbol: KEY_{name}
+// literal: LIT_{name} //name of literal type, not the value
+// operator: OP_{name}
+// identifier: IDENTIFIER
+
 enum Token
 {
     TOKEN, //INVALID
-    BEGIN, END, 
-    FLOAT, INT,
-    
+    KEY_BEGIN, KEY_END, 
+    LIT_FLOAT, LIT_INT,
+    KEY_TERMINATOR
 };
 
 #endif
