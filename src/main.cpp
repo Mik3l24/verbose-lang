105c936cf40074b0e53a802280256ac13ed6f36d
Mik3l24 <michalczus@gmail.com>
2023-05-20 18:57:19 +0200

    Removed the need for LLVM, made project ready for parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
 #include <cstdio>
 #include "lexer.h"
+#include "parser.hpp"
+
+extern int yyparse();
 
 int main(int argc, char** argv) 
 {
-    FILE* fd = stdin;
-    if(argc > 1 && (fd = fopen(argv[1], "r")) == NULL)
-        exit(EXIT_FAILURE);
-    if(fd == stdin) //Lexer only works when a file is open
-        exit(EXIT_FAILURE);
-    // create a lexer that consumes a file or reads stdin
-    Lexer lexer(fd);
-    // here we go!
-    lexer.lex();
-    // display the results
-    if(fd != stdin)
-        fclose(fd);
-    return 0;
+    yyparse();
 }
 
 // #include <iostream>
